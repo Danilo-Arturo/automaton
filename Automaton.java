6913a6b2c59dc02be99649730ad60dc47157d6a5
Danilo Pivaral <DaniloPivaral@MacBook-Pro-2.local>
2023-07-17 20:55:37 -0400

    Rewrite of conditional statements in update method to be terntary operation.

--- Automaton.java
+++ Automaton.java
@@ -22,8 +22,8 @@ public class Automaton
         this.numberOfCells = numberOfCells;
         state = new int[numberOfCells];
         // Seed the automaton with a single 'on' cell in the middle.
-        state[numberOfCells / 4] = 1;
-        state[1] = 1;
+        state[numberOfCells / 2] = 1;
+        //state[1] = 1;
         
     }
     
@@ -49,24 +49,36 @@ public class Automaton
     public void update()
     {
         // Build the new state in a separate array.
+        //WHY DO WE CREATE A NEW ARRAY?
+        // --->BECASUE state = nextState;
         int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
             int left, center, right;
+            
+            left = (i == 0 ? 0 : state[i - 1]);
+            /**
             if(i == 0) {
                 left = 0;
             }
             else {
                 left = state[i - 1];
             }
+            */
+            
             center = state[i];
+            
+            right = (i + 1 < state.length ? state[i + 1]: 0);
+            /**
             if(i + 1 < state.length) {
                 right = state[i + 1];
             }
             else {
                 right = 0;
             }
+            */
+            
             nextState[i] = (left + center + right) % 2;
         }
         state = nextState;
